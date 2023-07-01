4ac3676c12c400f0c9bcde3e0984b4d7eb434c37
Mateusz <61660055+yoggys@users.noreply.github.com>
2023-07-01 19:01:28 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (cwd / "README.md").read_text()
 
 setup(
     name="unbelievaboat",
-    version="1.0.3",
+    version="1.0.4",
     author="yoggys",
     author_email="yoggies@yoggies.ovh",
     description="Wrapper for UnbelievaBoat API.",
